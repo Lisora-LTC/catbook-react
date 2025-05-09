74e28447ac537a06d1573b180980f0b9f6b0bb8e
Lisora <2650611142@qq.com>
2025-05-09 13:53:38 +0800

    happinessCountFinished

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../utilities.css";
 import "./Profile.css";
+import CatHappiness from "../modules/CatHappiness.jsx";
 
 const Profile = () => {
+  const [catHappiness, setCatHappiness] = useState(0);
   return (
     <div>
       <div className="Profile-avatarContainer">
         <div className="Profile-avatar" />
       </div>
-      <h1 className="Profile-name u-textCenter">YOUR NAME HERE</h1>
+      <h1 className="Profile-name u-textCenter">Lisora</h1>
       <hr className="Profile-line" />
       <div className="u-flex">
         <div className="Profile-subContainer u-textCenter">
@@ -18,8 +20,12 @@ const Profile = () => {
           </div>
         </div>
         <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">CatHappiness</h4>
+          <CatHappiness catHappiness={catHappiness} />
+        </div>
+        <div className="Profile-subContainer u-textCenter">
           <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
-          <div id="favorite-cat">corgi</div>
+          <div id="favorite-cat">柯基</div>
         </div>
       </div>
     </div>
